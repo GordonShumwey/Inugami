929f6445e052538e8c0555e3f79d1c16cd66d07d
Rodrigo <r.serrano.rieloff@gmail.com>
2022-06-15 16:47:16 -0400

    actualizacion pantalla 1 y 2 del admin

--- app/src/main/java/com/taller/ngen/Admin.kt
+++ app/src/main/java/com/taller/ngen/Admin.kt
@@ -1,11 +1,46 @@
 package com.taller.ngen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Admin : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+
+        setOption()
+
+    }
+
+    private fun setOption(){
+        title = "Bienvenid@ a la sección para administradores"
+
+        val bundle = intent.extras
+        var email = bundle?.getString("email")
+
+        var emailName = findViewById<TextView>(R.id.nameUser)
+        var btnOtra = findViewById<Button>(R.id.btnOtrAdmin)
+
+        db.collection("users").document(email.toString()).get()
+            .addOnSuccessListener {
+                var nameUserAdmin = it.data?.get("nombre").toString()
+                emailName.setText(nameUserAdmin)
+            }
+
+        btnOtra.setOnClickListener(){
+            val goAdmin2 = Intent(this, AdminPantalla2::class.java).apply {
+                putExtra("email", email.toString())
+
+            }
+            startActivity(goAdmin2)
+
+        }
     }
 }
--- app/src/main/java/com/taller/ngen/AdminPantalla2.kt
+++ app/src/main/java/com/taller/ngen/AdminPantalla2.kt
@@ -2,10 +2,33 @@ package com.taller.ngen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AdminPantalla2 : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_pantalla2)
+
+        fundAdmin2()
+    }
+
+    private fun fundAdmin2(){
+        title="Otras Funciones del Administrador"
+
+        val bundle2 = intent.extras
+        var email2 = bundle2?.getString("email")
+
+        var nameUser2 = findViewById<TextView>(R.id.nameUser2)
+
+
+        db.collection("users").document(email2.toString()).get()
+            .addOnSuccessListener {
+                var name2 = it.data?.get("nombre").toString()
+                nameUser2.setText(name2)
+            }
     }
 }
